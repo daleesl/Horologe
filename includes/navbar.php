26091848c08aa2a171e78a89e0cb848f7671282e
daleesl <daleandrewslee@gmail.com>
2026-01-02 21:46:58 +0800

    updated navbar responsiveness

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,40 +1,40 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-black border-bottom border-secondary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-black sticky-top border-bottom border-secondary">
     <div class="container-fluid position-relative px-4 py-3">
         <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
             aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand d-lg-none me-auto display-1" href="index.php">HOROLOGE</a>
+        <a class="navbar-brand d-lg-none me-auto text-white display-1" href="index.php">HOROLOGE</a>
 
         <div class="d-flex px-lg-5 px-1 align-items-center order-lg-2">
-            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2 text-white" aria-label="Search">
-                <i class="bi bi-search"></i>
+            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2" aria-label="Search">
+                <i class="bi bi-search text-white"></i>
             </a>
-            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2 text-white" aria-label="Bag">
-                <i class="bi bi-bag"></i>
+            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2" aria-label="Bag">
+                <i class="bi bi-bag text-white"></i>
             </a>
-            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2 text-white" aria-label="Account">
-                <i class="bi bi-person"></i>
+            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2" aria-label="Account">
+                <i class="bi bi-person text-white"></i>
             </a>
         </div>
 
         <div class="collapse navbar-collapse order-lg-1" id="navbarContent">
             <ul class="navbar-nav me-lg-auto mb-2 mb-lg-0">
-                <li class="nav-item px-lg-4 px-2">
+                <li class="nav-item px-lg-2 px-md-1 px-0">
                     <a class="nav-link text-white" href="index.php">HOME</a>
                 </li>
-                <li class="nav-item px-lg-4 px-2">
+                <li class="nav-item px-lg-2 px-md-1 px-0">
                     <a class="nav-link text-white" href="collections.php">COLLECTIONS</a>
                 </li>
-                <li class="nav-item px-lg-4 px-2">
+                <li class="nav-item px-lg-2 px-md-1 px-0">
                     <a class="nav-link text-white" href="heritage.php">HERITAGE</a>
                 </li>
             </ul>
         </div>
 
         <a class="navbar-brand position-absolute top-50 start-50 translate-middle d-none d-lg-block" href="index.php">
-            <span>HOROLOGE</span>
+            <span class="text-white">HOROLOGE</span>
         </a>
     </div>
 </nav>
