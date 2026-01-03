ea1dcf7be7e25fa5d0b44f26d42791753dc3a863
daleesl <daleandrewslee@gmail.com>
2026-01-03 22:56:48 +0800

    added a json format to be populated in index.php

---
+++ assets/js/sample-products.js
@@ -0,0 +1,30 @@
+var products = [
+  {
+    "id": 1,
+    "name": "Rolex Cosmograph Platinum",
+    "category": "CLASSIC HERITAGE",
+    "price": 12400,
+    "image": "../assets/images/products/rolex/rolex-1.png"
+  },
+    {
+    "id": 2,
+    "name": "Rolex Sky Dweller",
+    "category": "GRAND SPORT",
+    "price": 9800,
+    "image": "../assets/images/products/rolex/rolex-2.png"
+    },
+    {
+    "id": 3,
+    "name": "Cartier Santos Green Arab",
+    "category": "CLASSIC HERITAGE",
+    "price": 15000,
+    "image": "../assets/images/products/cartier/cartier-1.png"
+    },
+    {
+    "id": 4,
+    "name": "Cartier Galbee",
+    "category": "LIMITED EDITIONS",
+    "price": 22000,
+    "image": "../assets/images/products/cartier/cartier-2.png"
+    }
+];
