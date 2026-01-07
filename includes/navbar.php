db05094cb159c4a4a78c243d783bb9b2dfad4604
daleesl <daleandrewslee@gmail.com>
2026-01-07 20:06:42 +0800

    enhance navigation bar styling and structure

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,43 +1,60 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-black sticky-top border-bottom border-secondary" style="z-index: 1030;">
-    <div class="container-fluid position-relative px-4 py-3">
-        <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
-            aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-dark bg-black sticky-top" style="z-index: 1030;">
+    <div class="container-fluid position-relative px-4 py-3 border-bottom border-secondary d-flex align-items-center">
+        <button class="navbar-toggler d-md-none me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLinks"
+            aria-controls="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand d-lg-none me-auto text-white display-1" href="index.php">HOROLOGE</a>
+        <!-- Mobile brand -->
+        <a class="navbar-brand d-lg-none me-auto text-white font-primary" href="index.php">HOROLOGE</a>
 
-        <div class="d-flex px-lg-5 px-1 align-items-center order-lg-2">
-            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2" aria-label="Search">
+        <!-- Desktop centered brand -->
+        <a class="navbar-brand position-absolute top-50 start-50 translate-middle d-none d-lg-block" href="index.php">
+            <span class="display-6 header font-primary text-white">HOROLOGE</span>
+        </a>
+
+        <!-- Right icons -->
+        <div class="d-flex align-items-center ms-auto">
+            <a href="#" class="text-decoration-none icon-link px-3" aria-label="Search">
                 <i class="bi bi-search text-white"></i>
             </a>
-            <a href="cart.php" class="text-decoration-none icon-link px-lg-4 px-2 position-relative" aria-label="Bag">
+            <a href="cart.php" class="text-decoration-none icon-link px-3 position-relative" aria-label="Bag">
                 <i class="bi bi-bag text-white"></i>
                 <span id="cartBadge" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="font-size: 0.65rem;">
                     <span id="cartCount">0</span>
                 </span>
             </a>
-            <a href="#" class="text-decoration-none icon-link px-lg-4 px-2" aria-label="Account">
+            <a href="#" class="text-decoration-none icon-link px-3" aria-label="Account">
                 <i class="bi bi-person text-white"></i>
             </a>
         </div>
+    </div>
 
-        <div class="collapse navbar-collapse order-lg-1" id="navbarContent">
-            <ul class="navbar-nav me-lg-auto mb-2 mb-lg-0">
-                <li class="nav-item px-lg-2 px-md-1 px-0">
-                    <a class="nav-link text-white" href="index.php">HOME</a>
-                </li>
-                <li class="nav-item px-lg-2 px-md-1 px-0">
-                    <a class="nav-link text-white" href="collections.php">COLLECTIONS</a>
-                </li>
-                <li class="nav-item px-lg-2 px-md-1 px-0">
-                    <a class="nav-link text-white" href="heritage.php">HERITAGE</a>
-                </li>
-            </ul>
+    <!-- Bottom row: desktop/tablet always visible -->
+    <div class="w-100 border-secondary d-none d-md-block align-items-center">
+        <div class="container-fluid px-4 py-2 d-flex justify-content-center">
+            <a class="nav-link text-secondary small p-0 mx-4" href="index.php">HOME</a>
+            <a class="nav-link text-secondary small p-0 mx-4" href="collections.php">COLLECTIONS</a>
+            <a class="nav-link text-secondary small p-0 mx-4" href="heritage.php">HERITAGE</a>
         </div>
+    </div>
 
-        <a class="navbar-brand position-absolute top-50 start-50 translate-middle d-none d-lg-block" href="index.php">
-            <span class="text-white display-6">HOROLOGE</span>
-        </a>
+    <!--mobile collapsible -->
+    <div class="w-100 border-top border-secondary d-md-none">
+        <div class="container-fluid">
+            <div class="collapse" id="navbarLinks">
+                <ul class="navbar-nav justify-content-center py-2 mx-auto">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-secondary small" href="index.php">HOME</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-secondary small" href="collections.php">COLLECTIONS</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-secondary small" href="heritage.php">HERITAGE</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </nav>
